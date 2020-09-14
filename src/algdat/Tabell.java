3f93b18908393b5d73ce241b91322c39e548ccf3
Erik Storås Sommer <eriksommer99@gmail.com>
2020-09-14 21:57:53 +0200

    uke 3 påbegynt

--- src/algdat/Tabell.java
+++ src/algdat/Tabell.java
@@ -5,7 +5,8 @@ import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class Tabell {
-    private Tabell(){}
+    private Tabell() {
+    }
 
     public static void bytt(int[] a, int i, int j) {
         int temp = a[i];
@@ -13,15 +14,15 @@ public class Tabell {
         a[j] = temp;
     }
 
-    public static int[] randPerm(int n){
+    public static int[] randPerm(int n) {
         Random r = new Random();
         int[] a = new int[n];
 
         Arrays.setAll(a, i -> i + 1);
 
-        for (int k = n - 1; k > 0; k--){
-            int i = r.nextInt(k +1);
-            bytt(a,k,i);
+        for (int k = n - 1; k > 0; k--) {
+            int i = r.nextInt(k + 1);
+            bytt(a, k, i);
         }
         return a;
     }
@@ -29,21 +30,21 @@ public class Tabell {
     public static void randPerm(int[] a) {
         Random r = new Random();
 
-        for (int k=a.length-1;k>0;k--) {
+        for (int k = a.length - 1; k > 0; k--) {
             int i = r.nextInt(k + 1); // tilfeldig tall fra [0,k]
-            bytt(a,k,i);
+            bytt(a, k, i);
         }
     }
 
-    public static int maks(int[] a, int fra, int til){
+    public static int maks(int[] a, int fra, int til) {
 
         fratilKontroll(a.length, fra, til);
 
         int m = fra;
         int maksverdi = a[fra];
 
-        for (int i = fra + 1; i < til; i++){
-            if (a[i] > maksverdi){
+        for (int i = fra + 1; i < til; i++) {
+            if (a[i] > maksverdi) {
                 m = i;
                 maksverdi = a[m];
             }
@@ -51,20 +52,20 @@ public class Tabell {
         return m;
     }
 
-    public static int maks(int[] a){
-        return maks(a, 0,a.length);
+    public static int maks(int[] a) {
+        return maks(a, 0, a.length);
     }
 
-    public static int min(int[] a, int fra, int til){
-        if (fra < 0 || til > a.length || fra >= til){
+    public static int min(int[] a, int fra, int til) {
+        if (fra < 0 || til > a.length || fra >= til) {
             throw new IllegalArgumentException("Illegalt intervall");
         }
 
         int m = fra;
         int minvedi = a[fra];
 
-        for (int i = fra + 1; i < til; i++){
-            if (a[i] < minvedi){
+        for (int i = fra + 1; i < til; i++) {
+            if (a[i] < minvedi) {
                 m = i;
                 minvedi = a[m];
             }
@@ -73,53 +74,53 @@ public class Tabell {
 
     }
 
-    public static int min(int[] a){
-        return min(a, 0,a.length);
+    public static int min(int[] a) {
+        return min(a, 0, a.length);
     }
 
-    public static void bytt(char[] c, int i, int j){
+    public static void bytt(char[] c, int i, int j) {
         char temp = c[i];
         c[i] = c[j];
         c[j] = temp;
     }
 
-    public static void skriv(int[] a, int fra, int til){
-        for (int i = fra; i <= til; i++){
+    public static void skriv(int[] a, int fra, int til) {
+        for (int i = fra; i <= til; i++) {
             System.out.print(a[i] + " ");
         }
     }
 
-    public static void skriv(int[] a){
-        for (int i : a){
+    public static void skriv(int[] a) {
+        for (int i : a) {
             System.out.print(i + " ");
         }
     }
 
-    public static void skrivln(int[] a, int fra, int til){
+    public static void skrivln(int[] a, int fra, int til) {
         skriv(a, fra, til);
         System.out.println();
     }
 
-    public static void skrivln(int[] a){
+    public static void skrivln(int[] a) {
         skriv(a);
         System.out.println();
     }
 
-    public static void fratilKontroll(int tabellengde, int fra, int til){
+    public static void fratilKontroll(int tabellengde, int fra, int til) {
 
-        if (tabellengde == 0){
+        if (tabellengde == 0) {
             throw new IllegalArgumentException("Tabellengden er null");
         }
 
-        if (fra < 0){
+        if (fra < 0) {
             throw new ArrayIndexOutOfBoundsException("fra(" + fra + ") er negativ!");
         }
 
-        if (til > tabellengde){
+        if (til > tabellengde) {
             throw new ArrayIndexOutOfBoundsException("til(" + til + ") > tablengde(" + tabellengde + ")");
         }
 
-        if (fra > til){
+        if (fra > til) {
             throw new ArrayIndexOutOfBoundsException("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
         }
 
@@ -129,19 +130,19 @@ public class Tabell {
     }
 
     public static void vhKontroll(int tablengde, int v, int h) {
-        if (v<0){
+        if (v < 0) {
             throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
         }
-        if (h >= tablengde){
+        if (h >= tablengde) {
             throw new ArrayIndexOutOfBoundsException("h(" + h + ") >= tablengde(" + tablengde + ")");
         }
 
-        if (v>h+1){
+        if (v > h + 1) {
             throw new IllegalArgumentException("v = " + v + ", h = " + h);
         }
     }
 
-    public static int[] nestMaks(int[] a){
+    public static int[] nestMaks(int[] a) {
         int n = a.length;
 
         if (n < 2) throw new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
@@ -150,19 +151,19 @@ public class Tabell {
 
         int nm;
 
-        if (m == 0){
+        if (m == 0) {
             nm = maks(a, 1, n);
-        }else if (m == n - 1){
-            nm = maks(a, 0, n -1);
-        }else {
+        } else if (m == n - 1) {
+            nm = maks(a, 0, n - 1);
+        } else {
             int mv = maks(a, 0, m);
             int mh = maks(a, m + 1, n);
             nm = a[mh] > a[mv] ? mh : mv;
         }
-        return new int[] {m, nm};
+        return new int[]{m, nm};
     }
 
-    public static int[] nestMaksSF(int[] a){
+    public static int[] nestMaksSF(int[] a) {
 
         int n = a.length;
 
@@ -176,10 +177,10 @@ public class Tabell {
 
         bytt(a, m, 0);
 
-        return new int[] {m, nm};
+        return new int[]{m, nm};
     }
 
-    public static int[] nestMaksSB(int[] a){
+    public static int[] nestMaksSB(int[] a) {
 
         int n = a.length;
 
@@ -193,16 +194,16 @@ public class Tabell {
 
         bytt(a, m, n - 1);
 
-        return new int[] {m, nm};
+        return new int[]{m, nm};
     }
 
-    public static void sortering(int[] a){
+    public static void sortering(int[] a) {
 
         int n = a.length;
 
         if (n < 2) throw new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
 
-        for (int i = n-1; i > 0; i--){
+        for (int i = n - 1; i > 0; i--) {
 
             int m = maks(a, 0, i);
 
@@ -210,79 +211,168 @@ public class Tabell {
 
             int nm = maks(a, 0, i);
 
-            bytt(a, nm, i-1);
+            bytt(a, nm, i - 1);
         }
     }
 
-    public static int[] nestMaksTR(int[] a){ //Turneringstre
+    public static int[] nestMaksTR(int[] a) { //Turneringstre
         int n = a.length;
 
         if (n < 2) throw new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
 
-        int[] b = new int[2*n];
+        int[] b = new int[2 * n];
         //System.arraycopy(a,0,b,n,n);
-        kopier(a,0,b,n,n);
+        kopier(a, 0, b, n, n);
 
-        for (int k = 2*n-2; k > 1; k -= 2){
-            b[k/2] = Math.max(b[k],b[k+1]);
+        for (int k = 2 * n - 2; k > 1; k -= 2) {
+            b[k / 2] = Math.max(b[k], b[k + 1]);
         }
 
         int maksverdi = b[1];
         int nestmaksverdi = Integer.MIN_VALUE;
 
-        for (int m = 2*n -1, k = 2; k < m; k *= 2){
-            int temp = b[k+1];
-            if (maksverdi != b[k]){
+        for (int m = 2 * n - 1, k = 2; k < m; k *= 2) {
+            int temp = b[k + 1];
+            if (maksverdi != b[k]) {
                 temp = b[k];
                 k++;
             }
-            if (temp > nestmaksverdi){
+            if (temp > nestmaksverdi) {
                 nestmaksverdi = temp;
             }
         }
-        return new int[] {maksverdi, nestmaksverdi};
+        return new int[]{maksverdi, nestmaksverdi};
     }
 
-    public static void kopier(int[] a, int i, int[] b, int j, int ant){
+    public static void kopier(int[] a, int i, int[] b, int j, int ant) {
         int aIndex = i;
-        for (int k = j; k < ant*2; k++){
-            if (aIndex < a.length){
+        for (int k = j; k < ant * 2; k++) {
+            if (aIndex < a.length) {
                 b[k] = a[aIndex];
                 aIndex++;
             }
         }
     }
 
-    public static void snu(int[] a, int v, int h){
-        while (v < h){
+    public static void snu(int[] a, int v, int h) {
+        while (v < h) {
             bytt(a, v++, h--);
         }
     }
 
-    public static void snu(int[] a, int v){
-        snu(a, v, a.length-1);
+    public static void snu(int[] a, int v) {
+        snu(a, v, a.length - 1);
     }
 
-    public static void snu(int[] a){
-        snu(a, 0, a.length-1);
+    public static void snu(int[] a) {
+        snu(a, 0, a.length - 1);
     }
 
-    public static boolean nestePermutasjon(int[] a){
-        int i = a.length -2;
-        while (i >= 0 && a[i] > a[i +1]){
+    public static boolean nestePermutasjon(int[] a) {
+        int i = a.length - 2;
+        while (i >= 0 && a[i] > a[i + 1]) {
             i--;
         }
-        if (i < 0){
+        if (i < 0) {
             return false;
         }
 
-        int j = a.length -1;
-        while (a[j] < a[i]){
+        int j = a.length - 1;
+        while (a[j] < a[i]) {
             j--;
         }
-        bytt(a,i,j);
-        snu(a,i + 1);
+        bytt(a, i, j);
+        snu(a, i + 1);
         return true;
     }
 
+    public static int binarySearch(int verdi, int[] a, int v, int h) {
+        int pivot = (v + h) / 2;
+
+        if (h - v == 0) {
+            if (a[pivot] == verdi) {
+                return pivot;
+            } else {
+                return -pivot;
+            }
+        }
+
+        if (a[pivot] <= verdi) {
+            if (a[pivot] == verdi) {
+                return pivot;
+            }
+            return binarySearch(verdi, a, pivot + 1, h);
+        } else {
+            return binarySearch(verdi, a, v, pivot - 1);
+        }
+    }
+
+    public static int inversjoner(int[] a){
+        int antall = 0;
+        for (int i = 0; i < a.length-1; i++){
+            for (int j = i+1;j < a.length; j++){
+                if (a[i] > a[j]){
+                    antall++;
+                }
+            }
+        }
+        return antall;
+    }
+
+    public static void utvalgssortering(int[] a){
+        for (int i = 0; i < a.length-1; i++){
+            bytt(a, i, min(a,i,a.length));
+        }
+    }
+
+    public static void utvalgssortering(int[] a, int fra, int til){
+
+        fratilKontroll(a.length, fra, til);
+
+        for (int i = fra; i < a.length - 1; i++) {
+            int index = i;
+            for (int j = til + 1; j < a.length; j++){
+                if (a[j] < a[index]){
+                    index = j;
+                }
+            }
+            int smallerNumber = a[index];
+            a[index] = a[i];
+            a[i] = smallerNumber;
+        }
+    }
+
+    //Hentet fra https://www.javatpoint.com/selection-sort-in-java
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j; //searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+
+    public static int usortertsok(int[] a, int verdi){
+        for (int i = 0; i < a.length; i++){
+            if (a[i] == verdi){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int linearsok(int[] a, int verdi){
+        if (a.length == 0 || verdi > a[a.length-1]){
+            return -(a.length +1);
+        }
+        int i = 0;
+        for ( ; a[i] < verdi; i++);
+
+        return verdi == a[i] ? i : -(i +1);
+    }
 }
