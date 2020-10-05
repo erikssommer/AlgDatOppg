ab0aef15ae18c61eadcbf7c16ba840054c0878fe
Erik Storås Sommer <eriksommer99@gmail.com>
2020-10-05 18:46:21 +0200

    Seksjon 421 og oppsettet til køer ferdig

---
+++ src/algdat/hjelpeklasser/Ko.java
@@ -0,0 +1,10 @@
+package algdat.hjelpeklasser;
+
+public interface Ko<T> {
+    public boolean leggInn(T verdi);
+    public T kikk();
+    public T taUt();
+    public int antall();
+    public boolean tom();
+    public void nullstill();
+}
