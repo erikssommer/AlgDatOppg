706bafa94a84dcf1ef250dd24b4540019c9b9637
Erik Storås Sommer <eriksommer99@gmail.com>
2020-11-06 14:16:45 +0100

    Jobbet videre med prioritetskø

---
+++ src/algdat/hjelpeklasser/HeapPrioritetsKo.java
@@ -0,0 +1,96 @@
+package algdat.hjelpeklasser;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class HeapPrioritetsKo<T> implements PrioritetsKo<T> {
+    private T[] heap;
+    private int antall;
+    private Comparator<? super T> comp;
+
+    @SuppressWarnings("unchecked")
+    public HeapPrioritetsKo(int kapasitet, Comparator<? super T> comp) {
+        if (kapasitet < 0) throw new IllegalArgumentException("Negativ kapasitet er ikke mulig");
+
+        heap = (T[]) new Object[kapasitet+1];
+        antall = 0;
+        this.comp = comp;
+    }
+
+    public HeapPrioritetsKo(Comparator<? super T> comp){
+        this(8, comp);
+    }
+
+    public static <T extends Comparable<? super T>> HeapPrioritetsKo<T> naturligOrden(int kapasitet){
+        return new HeapPrioritetsKo<>(kapasitet, Comparator.naturalOrder());
+    }
+
+    public static <T extends Comparable<? super T>> HeapPrioritetsKo<T> naturligOrden(){
+        return naturligOrden(8);
+    }
+
+    @Override
+    public void leggInn(T verdi) {
+        Objects.requireNonNull(verdi, "Verdi er null!");
+
+        if (++antall == heap.length) heap = Arrays.copyOf(heap, 2*antall);
+
+        int k = antall;
+        heap[0] = verdi;
+
+        while (comp.compare(verdi, heap[k/2]) < 0){
+            heap[k] = heap[k/2];
+            k /= 2;
+        }
+        heap[0] = null;
+        heap[k] = verdi;
+    }
+
+    @Override
+    public T kikk() {
+        if (tom()) throw new NoSuchElementException("Køen er tom!");
+        return heap[1];
+    }
+
+    @Override
+    public T taUt() {
+        return null;
+    }
+
+    @Override
+    public boolean taUt(T verdi) {
+        return false;
+    }
+
+    @Override
+    public int antall() {
+        return 0;
+    }
+
+    @Override
+    public boolean tom() {
+        return false;
+    }
+
+    @Override
+    public void nullstill() {
+        for (int i = 0; i <= antall; i++) heap[i] = null;
+        antall = 0;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+
+        if (antall > 0) sb.append(heap[1]);
+
+        for (int i = 2; i <= antall; i++) sb.append(',').append(' ').append(heap[i]);
+
+        sb.append(']');
+
+        return sb.toString();
+    }
+}
---
+++ src/algdat/ukeoppgaver/uke10/seksjon533/Oppgave1.java
@@ -0,0 +1,17 @@
+package algdat.ukeoppgaver.uke10.seksjon533;
+
+import algdat.hjelpeklasser.HeapPrioritetsKo;
+import algdat.hjelpeklasser.PrioritetsKo;
+
+public class Oppgave1 {
+    public static void main(String[] args) {
+        int[] a = {3,5,7,10,5,8,18,12,17,11,10,14};
+        PrioritetsKo<Integer> ko = HeapPrioritetsKo.naturligOrden();
+
+        for (int k : a) ko.leggInn(k);
+
+        ko.leggInn(6); System.out.println(ko);
+        ko.leggInn(10); System.out.println(ko);
+        ko.leggInn(12); System.out.println(ko);
+    }
+}
