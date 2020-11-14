1e042c4183793584248df5fcc1b96f68cadf3ec6
Erik Storås Sommer <eriksommer99@gmail.com>
2020-11-14 23:35:12 +0100

    Enkel bruk av lambda-uttrykk

---
+++ src/algdat/diverse/Lambda.java
@@ -0,0 +1,34 @@
+package algdat.diverse;
+
+/**
+ * Enkel bruk av lambda-uttrykk
+ */
+
+public class Lambda {
+
+    interface MyInterface{
+        public void sum(int a, int b);
+    }
+
+    static class Demo implements MyInterface{
+
+        @Override
+        public void sum(int a, int b) {
+            System.out.println("Summen er " + (a + b));
+        }
+    }
+
+    static class TestWithoutLambda{
+        public static void main(String[] args) {
+            MyInterface demo = new Demo();
+            demo.sum(10, 20);
+        }
+    }
+
+    static class TestWithLambda{
+        public static void main(String[] args) {
+            MyInterface lambda = (a, b) -> System.out.println("Summen er " + (a + b));
+            lambda.sum(10,20);
+        }
+    }
+}
