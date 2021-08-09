6a93919aa809679d9d195db6f79ad754cd5dadee
Erik Storås Sommer <erikssommer@gmail.com>
2021-08-09 19:40:39 +0200

    Formating

--- src/algdat/en/Collection.java
+++ src/algdat/en/Collection.java
@@ -1,14 +1,15 @@
 package algdat.en;
 
 public class Collection {
-    private Collection(){}
+    private Collection() {
+    }
 
-    public static int max(int[] a, int from, int to){
+    public static int max(int[] a, int from, int to) {
         int maxindex = from;
         int maxvalue = a[from];
 
-        for (int i = from + 1; i < to; i++){
-            if (a[i] > maxvalue){
+        for (int i = from + 1; i < to; i++) {
+            if (a[i] > maxvalue) {
                 maxindex = i;
                 maxvalue = a[i];
             }
@@ -16,12 +17,12 @@ public class Collection {
         return maxindex;
     }
 
-    public static int min(int[] a, int from, int to){
+    public static int min(int[] a, int from, int to) {
         int minindex = from;
         int minvalue = a[from];
 
-        for (int i = from + 1; i < to; i++){
-            if (a[i] > minvalue){
+        for (int i = from + 1; i < to; i++) {
+            if (a[i] > minvalue) {
                 minindex = i;
                 minvalue = a[i];
             }
@@ -29,11 +30,11 @@ public class Collection {
         return minindex;
     }
 
-    public static <T extends Comparable<? super T>> int maks(T[] a){
+    public static <T extends Comparable<? super T>> int maks(T[] a) {
         int index = 0;
         T value = a[index];
 
-        for (int i = 1; i < a.length; i++){
+        for (int i = 1; i < a.length; i++) {
             if (a[i].compareTo(value) > 0) {
                 value = a[i];
                 index = i;
